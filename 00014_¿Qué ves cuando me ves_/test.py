229b37edbf1a1d5be90a00aa4d612d58d893a4b4
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-26 22:23:48 +0000

    Mumuki Export on 2019-04-26 22:23:48 +0000

--- 00014_¿Qué ves cuando me ves_/test.py
+++ 00014_¿Qué ves cuando me ves_/test.py
@@ -1,7 +1,4 @@
 
 
-  def test_description_example(self):
-    self.assertTrue(True)
-  
   def stranger_things_no_es_una_serie_recomendable(self):
     self.assertTrue(serie_no_recomendable("Stranger things"))
