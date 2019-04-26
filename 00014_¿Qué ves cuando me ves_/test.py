3d305f9145bb6476b5c23f255e918720662a8cdb
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-26 22:39:13 +0000

    Mumuki Export on 2019-04-26 22:39:13 +0000

--- 00014_¿Qué ves cuando me ves_/test.py
+++ 00014_¿Qué ves cuando me ves_/test.py
@@ -1,15 +1,3 @@
 
-  def stranger_things_no_es_recomendable(self):
+  def bla(self):
     self.assertTrue(serie_no_recomendable("Stranger things"))
-    
-  def el_marginal_no_es_recomendable(self):
-    self.assertTrue(serie_no_recomendable("El marginal"))
-    
-  def veinticuatro_no_es_recomendable(self):
-    self.assertTrue(serie_no_recomendable("24"))
-    
-  def breaking_bad_es_recomendable(self):
-    self.assertFalse(serie_no_recomendable("Breaking bad"))
-    
-  def los_simpsons_es_recomendable(self):
-    self.assertFalse(serie_no_recomendable("Los simpsons"))
