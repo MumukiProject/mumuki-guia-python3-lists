ff6c82b45ccac516772a9e25494d2f667e0ed07c
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-26 22:34:53 +0000

    Mumuki Export on 2019-04-26 22:34:53 +0000

--- 00014_¿Qué ves cuando me ves_/test.py
+++ 00014_¿Qué ves cuando me ves_/test.py
@@ -1,6 +1,5 @@
 
   def stranger_things_no_es_una_serie_recomendable(self):
-    global ranking
-    self.assertTrue(serie_no_recomendable("Stranger things"))
+    self.assertTrue(True))
     
     
