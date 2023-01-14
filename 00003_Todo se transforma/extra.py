2d4cb35e2f8293dcdaad7f67f449003da3ab78e2
franco@mumuki.org <franco@mumuki.org>
2023-01-14 15:25:53 +0000

    Mumuki Export on 2023-01-14 15:25:53 +0000

--- 00003_Todo se transforma/extra.py
+++ 00003_Todo se transforma/extra.py
@@ -1,8 +1,6 @@
 libros = ["Ensayo sobre la ceguera", "Socorro", "Mi planta naranja lima"]
 
 def listo():
-  if "Fundación" in libros:
-    return "¡Muy bien!"
-  else:
-    raise RuntimeError("No te olvides de agregar 'Fundación' a la lista de libros")
+  assert "Fundación" in libros, "No te olvides de agregar 'Fundación' a la lista de libros":
+  return "¡Muy bien!"
   
