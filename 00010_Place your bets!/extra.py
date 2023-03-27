21ca6117a55ff3c357192b8a45c950893cf4f57e
nadia@mumuki.org <nadia@mumuki.org>
2023-03-27 19:35:56 +0000

    Mumuki Export on 2023-03-27 19:35:56 +0000

--- 00010_Place your bets!/extra.py
+++ 00010_Place your bets!/extra.py
@@ -1,4 +1,4 @@
 lottery_numbers = [2, 11, 17, 32, 36, 39]
-coin_flips = [1, 6, 6, 2, 2, 4]
-die_rolls = [False, False, True, False]
+die_rolls = [1, 6, 6, 2, 2, 4]
+coin_flips = [False, False, True, False]
 list_of_lists = [[1, 2, 3], [4, 5, 6]]
